0762ed1306c48a80ac3abf06d423c22b04db84cb
ficoCT <fico.dev.ct@gmail.com>
2022-05-02 22:50:27 +0200

    React router - podstawowy routing projektu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
-import './App.css';
-import { Button } from 'react-bootstrap';
+import * as React from 'react';
+import { Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <Button variant="primary">Button #1</Button>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <div>
+            <h1>Egzamin z Wychowania Fizycznego</h1>
+            <nav style={{ borderBottom: 'solid 1px', paddingBottom: '1rem' }}>
+                <Link to="/exam">exam</Link> |{' '}
+                <Link to="/famale-exam">famale-exam</Link> {' '}
+                <Link to="/male-exam">male-exam</Link>
+                <Link to="/expenses">Expenses</Link>
+            </nav>
+        </div>
+    );
 }
 
-export default App;
+
+// import './App.css';
+// import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+// import Exam from "./components/Exam";
+// import MaleExam from "./components/MaleExam";
+// import FemaleExam from "./components/FemaleExam";
+//
+// function App() {
+//     return (
+//         <Router>
+//             <div>
+//                 <ul>
+//                     <li><Link to="/">Egzamin</Link></li>
+//                     <li><Link to="/about">Egzamin kobiety</Link></li>
+//                     <li><Link to="/topics">Egzamin mężczyźni</Link></li>
+//                     <li><Link to="/topics">Symulacja egzaminu</Link></li>
+//                 </ul>
+//
+//                 <hr />
+//
+//                 <Route exact path="/" component={Exam} />
+//                 <Route path="/about" component={MaleExam} />
+//                 <Route path="/topics" component={FemaleExam} />
+//             </div>
+//         </Router>
+//     );
+// }
+//
+// export default App;
+
--- src/index.js
+++ src/index.js
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Exam from "./components/Exam";
+import FemaleExam from "./components/FemaleExam";
+import MaleExam from "./components/MaleExam";
+import Expenses from "./components/routes/expenses";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<App />}>
+                  <Route path="exam" element={<Exam />} />
+                  <Route path="famale-exam" element={<FemaleExam />} />
+                  <Route path="male-exam" element={<MaleExam />} />
+                  <Route path="expenses" element={<Expenses />} />
+              </Route>
+          </Routes>
+      </BrowserRouter>
   </React.StrictMode>
 );
 
